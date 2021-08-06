f8281c3e88cbe4dc1becf5b734a1ad2feb00719c
Mike Nieva <m@mikenieva.com>
2021-08-06 10:41:41 -0500

    Commit de base

---
+++ index.js
@@ -0,0 +1,24 @@
+// 1 IMPORTACIONES
+
+const express = require('express')
+const connectDB = require('./config/db')
+
+const app = express()
+
+// 2 MIDDLEWARES
+
+// VARIABLES DE ENTORNO
+require('dotenv').config()
+
+
+
+// 3 RUTEO
+
+
+
+// 4 SERVIDOR
+app.listen(process.env.PORT, () => {
+    console.log(`Conectados al servidor en el puerto ${process.env.PORT}`)
+})
+
+
